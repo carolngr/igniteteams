7a6178f0a2280b4cbcf0de0b24be54e7567ca6f9
Caroline Nogueira <carol.ngr8@gmail.com>
2024-06-18 21:07:06 -0300

    feat: Add component card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 
 export function Groups() {
@@ -11,6 +12,8 @@ export function Groups() {
         title='Turmas' 
         subtitle='Jogue com a sua turma'
       />
+
+      <GroupCard title="Carolzinha feras" />
     </Container>
   );
 }
