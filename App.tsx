3e93a528f113a4e30e0046564f8af576a2cf6dad
Caroline Nogueira <carol.ngr8@gmail.com>
2024-06-23 23:17:27 -0300

    feat: Add new page Players and add component ButtonIcon

--- App.tsx
+++ App.tsx
@@ -9,12 +9,13 @@ import {
 
 import theme from './src/theme';
 
-import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
 
 import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
 
   return (
     <ThemeProvider theme={theme}>
@@ -23,7 +24,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-    {fontsLoaded ? <Groups /> : <Loading/> }
+    {fontsLoaded ? <Players /> : <Loading/> }
     </ThemeProvider>
   );
 }
